b3d9ad0a693ce4ca02ea6f1941c6b831f77d9dd1
大海 <iwangyang@qq.com>
2025-03-05 16:48:21 +0800

    http

--- constants/Nodes.ts
+++ constants/Nodes.ts
@@ -2,7 +2,7 @@ export default [
   // most
   'https://api.most.red',
   // pSea
-  // 'https://www.icewall.fun',
+  'http://106.13.22.90:1976',
   // damon
   'https://damon.most.red',
   // night sky
