07b5470c7034bbce1f96b7b7fe1ca351000063e9
大海 <iwangyang@qq.com>
2025-03-06 23:35:24 +0800

    dot.most.box

--- constants/Nodes.ts
+++ constants/Nodes.ts
@@ -2,7 +2,7 @@ export default [
   // most
   'https://api.most.red',
   // pSea
-  'http://106.13.22.90:1976',
+  'https://106.13.22.90:1976',
   // damon
   'https://damon.most.red',
   // night sky
