3167375b0a01fb782a0103e1c7ba3a75efa3905a
大海 <iwangyang@qq.com>
2025-03-16 01:08:59 +0800

    PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const config = withPWA({
+  dest: "public",
+  register: true,
+  workboxOptions: {
+    skipWaiting: true,
+  },
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
+
+export default config;
