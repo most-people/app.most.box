cd08dc2c9b97ebdd10efe8e93752d74ebfd00c8f
大海 <iwangyang@qq.com>
2025-03-15 23:00:55 +0800

    x

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import '@/global.css'
+import '@/main.css'
 import routes from '~react-pages'
 
 const router = createBrowserRouter(routes)
