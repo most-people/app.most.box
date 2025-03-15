3f5a549665517e9aa1e86535d5a67d64b8ccb487
大海 <iwangyang@qq.com>
2025-03-15 16:31:36 +0800

    wow

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/icons/mask-icon.svg'
 import './App.css'
 
 function App() {
--- vite.config.ts
+++ vite.config.ts
@@ -13,19 +13,22 @@ export default defineConfig({
         short_name: "Most.Box",
         description: "Most.Box Application",
         theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "/icons/pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/icons/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "/icons/pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
