fc5cf2333912fa8d3f76ae067209f89722196dbd
大海 <iwangyang@qq.com>
2025-03-16 19:50:04 +0800

    Mantine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,36 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import {
+  MantineProvider,
+  createTheme,
+  MantineColorsTuple,
+} from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import { ModalsProvider } from "@mantine/modals";
+import "@mantine/core/styles.css";
+import "@mantine/notifications/styles.css";
+
+const green: MantineColorsTuple = [
+  "#e6ffee",
+  "#d3f9e0",
+  "#a8f2c0",
+  "#7aea9f",
+  "#54e382",
+  "#3bdf70",
+  "#2bdd66",
+  "#1bc455",
+  "#0bae4a",
+  "#00973c",
+];
+
+const theme = createTheme({
+  primaryColor: "green",
+  colors: {
+    green,
+  },
+});
+
 import "@/app/global.css";
 
 const geistSans = Geist({
@@ -46,9 +77,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <MantineProvider defaultColorScheme="auto" theme={theme}>
+          <Notifications />
+          <ModalsProvider>{children}</ModalsProvider>
+        </MantineProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
+import { Button } from "@mantine/core";
 
 export default function HomePage() {
-  return <Link href="/login">Login</Link>;
+  return (
+    <>
+      <Button component={Link} href="/login">
+        Login
+      </Button>
+    </>
+  );
 }
