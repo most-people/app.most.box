eea5d5d4cc61dea2bfe562a5746f4ca759251cfb
大海 <iwangyang@qq.com>
2025-03-18 20:37:29 +0800

    icons svg

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,30 @@ import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   output: "export",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
 };
 
 const config = withPWA({
