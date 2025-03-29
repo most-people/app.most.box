4862f275c53f2ab1db2f80e546afb433219fc774
大海 <iwangyang@qq.com>
2025-03-29 19:55:26 +0800

    display: grid;

--- src/components/home/chat.tsx
+++ src/components/home/chat.tsx
@@ -63,80 +63,6 @@ export default function HomeChat() {
       lastMessage: "Let me know",
       time: "03 Jul",
     },
-    {
-      id: "1",
-      name: "赖雅娇",
-      avatar: "https://i.pravatar.cc/150?img=1",
-      lastMessage: "Totally, I do understand it",
-      time: "10:24",
-      unread: 1,
-    },
-    {
-      id: "2",
-      name: "张喜娟",
-      avatar: "https://i.pravatar.cc/150?img=2",
-      lastMessage: "Awesome!",
-      time: "9:28",
-      unread: 65,
-    },
-    {
-      id: "3",
-      name: "萧惠萍",
-      avatar: "https://i.pravatar.cc/150?img=3",
-      lastMessage: "Totally, I do understand it",
-      time: "Yesterday",
-    },
-    {
-      id: "4",
-      name: "林佩瑜",
-      avatar: "https://i.pravatar.cc/150?img=4",
-      lastMessage: "Hey there! 👋",
-      time: "03 Jul",
-    },
-    {
-      id: "5",
-      name: "吴世伟",
-      avatar: "https://i.pravatar.cc/150?img=5",
-      lastMessage: "Let me know",
-      time: "03 Jul",
-    },
-    {
-      id: "1",
-      name: "赖雅娇",
-      avatar: "https://i.pravatar.cc/150?img=1",
-      lastMessage: "Totally, I do understand it",
-      time: "10:24",
-      unread: 1,
-    },
-    {
-      id: "2",
-      name: "张喜娟",
-      avatar: "https://i.pravatar.cc/150?img=2",
-      lastMessage: "Awesome!",
-      time: "9:28",
-      unread: 65,
-    },
-    {
-      id: "3",
-      name: "萧惠萍",
-      avatar: "https://i.pravatar.cc/150?img=3",
-      lastMessage: "Totally, I do understand it",
-      time: "Yesterday",
-    },
-    {
-      id: "4",
-      name: "林佩瑜",
-      avatar: "https://i.pravatar.cc/150?img=4",
-      lastMessage: "Hey there! 👋",
-      time: "03 Jul",
-    },
-    {
-      id: "5",
-      name: "吴世伟",
-      avatar: "https://i.pravatar.cc/150?img=5",
-      lastMessage: "Let me know",
-      time: "03 Jul",
-    },
   ];
 
   return (
@@ -200,9 +126,14 @@ export default function HomeChat() {
       </Box>
 
       <Tabs.Panel value="friends">
-        <Stack gap="xs" mt="md">
+        <Box className="chats">
           {chatList.map((chat, index) => (
-            <Group key={index} wrap="nowrap" justify="space-between">
+            <Group
+              key={index}
+              wrap="nowrap"
+              justify="space-between"
+              className="chat"
+            >
               <Group wrap="nowrap">
                 <Avatar src={chat.avatar} size="lg" radius="md" />
                 <Box>
@@ -224,13 +155,18 @@ export default function HomeChat() {
               </Flex>
             </Group>
           ))}
-        </Stack>
+        </Box>
       </Tabs.Panel>
 
       <Tabs.Panel value="groups">
-        <Stack gap="xs" mt="md">
+        <Box className="chats">
           {chatList.map((chat, index) => (
-            <Group key={index} wrap="nowrap" justify="space-between">
+            <Group
+              key={index}
+              wrap="nowrap"
+              justify="space-between"
+              className="chat"
+            >
               <Group wrap="nowrap">
                 <Avatar src={chat.avatar} size="lg" radius="md" />
                 <Box>
@@ -252,7 +188,7 @@ export default function HomeChat() {
               </Flex>
             </Group>
           ))}
-        </Stack>
+        </Box>
       </Tabs.Panel>
     </Tabs>
   );
